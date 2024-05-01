97189523e487f32020f013676af0b365f8ba5d15
johnnysking <arkalomeco@hotmail.com>
2024-05-01 17:42:57 -0400

    frontend test

--- src/App.svelte
+++ src/App.svelte
@@ -1,26 +1,10 @@
 <script>
-  import { fly } from 'svelte/transition';
-  import Explosion from './lib/Explosion.svelte';
-  import Fade from './lib/Fade.svelte';
   import Homepage from './lib/Homepage.svelte';
-  
-  let explosionFinished = false;
-  let fadeFinished = false;
-
+  import Animation from './lib/Animation.svelte'  
 </script>
 
 <main>
-  {#if !explosionFinished}
-    <Explosion on:explosionFinish={() => explosionFinished = true} />
-  {:else}
-    {#if !fadeFinished}
-      <div transition:fly={{ y: 600, duration: 2000 }}>
-        <Fade on:animationFinish={() => console.log('finished')} />
-      </div>
-    {:else}
-     <!-- what comes next? -->
-     {/if}
-  {/if}
+  <Animation />
   <Homepage />
 </main>
 
---
+++ src/lib/Animation.svelte
@@ -0,0 +1,31 @@
+<script>
+    import { fly } from 'svelte/transition';
+    import Explosion from './Explosion.svelte';
+    import Fade from './Fade.svelte';
+    import { createEventDispatcher, onMount } from 'svelte';
+    let explosionFinished = false;
+    let fadeFinished = false;
+    const dispatch = createEventDispatcher();
+
+    onMount(() => {
+        setTimeout(() => {
+        dispatch('animationFinish');
+        }, 3000); // Adjust the delay time as needed
+    });
+</script>
+
+{#if !explosionFinished}
+    <Explosion on:explosionFinish={() => explosionFinished = true} />
+  {:else}
+    {#if !fadeFinished}
+      <div transition:fly={{ y: 600, duration: 2000 }}>
+        <Fade on:animationFinish={() => console.log('finished')} />
+      </div>
+    {:else}
+     <!-- what comes next? -->
+     {/if}
+  {/if}
+
+<style>
+
+</style>
