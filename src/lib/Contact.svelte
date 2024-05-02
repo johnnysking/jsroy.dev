b19ecf147c4d155b54e284dc7c6d37ac232309bd
johnnysking <arkalomeco@hotmail.com>
2024-05-02 19:28:28 -0400

    next

---
+++ src/lib/Contact.svelte
@@ -0,0 +1,77 @@
+<script>
+    let name = '';
+    let email = '';
+    let message = '';
+  
+    function handleSubmit() {
+      const formData = { name, email, message };
+      console.log(formData); // You can handle form submission here
+    }
+</script>
+  
+<div class="contact-form-container">
+    <form on:submit|preventDefault={handleSubmit}>
+        <div class="form-group">
+            <label for="name">Nom:</label>
+            <input type="text" id="name" bind:value={name} />
+        </div>
+  
+        <div class="form-group">
+            <label for="email">Courriel:</label>
+            <input type="email" id="email" bind:value={email} />
+        </div>
+  
+        <div class="form-group">
+            <label for="message">Message:</label>
+            <textarea id="message" bind:value={message}></textarea>
+        </div>
+  
+        <button type="submit">Submit</button>
+    </form>
+</div>
+  
+<style>
+    .contact-form-container {
+        max-width: 400px;
+        margin: 0 auto;
+        padding: 20px; /* Adjust this padding to create space */
+    }
+
+    .form-group {
+        margin-bottom: 20px;
+    }
+
+    label {
+        display: block;
+        font-weight: bold;
+        margin-bottom: 5px;
+    }
+
+    input[type="text"],
+    input[type="email"],
+    textarea {
+        width: 100%;
+        padding: 10px;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+        box-sizing: border-box;
+    }
+
+    textarea {
+        resize: vertical;
+    }
+
+    button {
+        background-color: #000000;
+        color: #fff;
+        padding: 10px 20px;
+        border: none;
+        border-radius: 5px;
+        cursor: pointer;
+    }
+
+    button:hover {
+        background-color: #fff;
+        color: #000000;
+    }
+</style>
--- src/lib/Homepage.svelte
+++ src/lib/Homepage.svelte
@@ -1,19 +1,31 @@
 <script>
-
-</script>
-
-<div>
-    <p>spécialiste en développement web personnalisé avec une solide expérience dans le domaine. Je propose des services de création de sites web, de portfolios et de boutiques en ligne sur mesure.
-
-        Mon approche est centrée sur vos besoins spécifiques et vos objectifs commerciaux. Je m'engage à vous fournir des solutions web de qualité qui reflètent votre identité et captivent votre public cible.
-        
-        N'hésitez pas à me contacter pour discuter de votre projet et découvrir comment je peux vous aider à atteindre vos objectifs en ligne.</p>
-</div>
-<div>
-    <button>Portfolio</button>
-    <button>Contact</button>
-</div>
-
-<style>
-
-</style>
+    import Contact from "./Contact.svelte";
+    
+    let showContact = false;
+  
+    function handleButtonClick() {
+      showContact = !showContact;
+    }
+  </script>
+  
+  <div>
+    <p>Spécialiste en développement web personnalisé avec une solide expérience dans le domaine. Je propose des services de création de sites web, de portfolios et de boutiques en ligne sur mesure.
+  
+      Mon approche est centrée sur vos besoins spécifiques et vos objectifs commerciaux. Je m'engage à vous fournir des solutions web de qualité qui reflètent votre identité et captivent votre public cible.
+          
+      N'hésitez pas à me contacter pour discuter de votre projet et découvrir comment je peux vous aider à atteindre vos objectifs en ligne.</p>
+  </div>
+  
+  <div>
+    <button on:click={handleButtonClick}>Portfolio</button>
+    <button on:click={handleButtonClick}>Contact</button>
+  </div>
+  
+  {#if showContact}
+    <Contact />
+  {/if}
+  
+  <style>
+    
+  </style>
+  
