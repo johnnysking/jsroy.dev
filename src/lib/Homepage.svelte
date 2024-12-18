0e5d493b7c8844d1d24a60a33e942b9b54d0e562
johnnysking <arkalomeco@hotmail.com>
2024-12-18 16:38:55 -0500

    test fix homepage

--- src/lib/Homepage.svelte
+++ src/lib/Homepage.svelte
@@ -84,9 +84,9 @@
 </style>
 
 <div>
-  <p>spécialiste en développement web personnalisé avec une solide expérience dans le domaine. Je propose des services de création de sites web, de portfolios et de boutiques en ligne sur mesure.
+  <p>J'accompagne mes clients dans la création de sites personnalisés, de portfolios ou de boutiques en ligne sur mesure.
 
-    Mon approche est centrée sur vos besoins spécifiques et vos objectifs commerciaux. Je m'engage à vous fournir des solutions web de qualité qui reflètent votre identité et captivent votre public cible.
+    Mon approche est centrée sur vos besoins spécifiques. Je m'engage à vous fournir des solutions web de qualité qui reflètent votre identité et captivent votre public cible.
     
     N'hésitez pas à me contacter pour discuter de votre projet et découvrir comment je peux vous aider à atteindre vos objectifs en ligne.</p>
 </div>
